81291d516e4b86095d26545ebdeb3924adf32f04
hasbisdqi <muhhasbiassidiqi18@gmail.com>
2024-10-02 21:19:30 +0700

    update sql

--- kelas praktikum/election.sql
+++ kelas praktikum/election.sql
@@ -1,37 +1,49 @@
+-- Create election database
+CREATE DATABASE election;
+-- Use election database
+USE election;
+
+-- Create tables
+
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE elections (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     start_date DATE NOT NULL,
-    end_date DATE NOT NULL
+    end_date DATE NOT NULL,
+    CONSTRAINT elections_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE candidates (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     bio TEXT NOT NULL,
     election_id INT NOT NULL,
-    FOREIGN KEY (election_id) REFERENCES elections(id)
-);
-
-CREATE TABLE votes (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    candidate_id INT NOT NULL,
-    voter_id INT NOT NULL,
-    FOREIGN KEY (candidate_id) REFERENCES candidates(id)
-    FOREIGN KEY (voter_id) REFERENCES voters(id)
+    CONSTRAINT candidates_pk PRIMARY KEY (id),
+    CONSTRAINT candidate_election FOREIGN KEY (election_id) REFERENCES elections(id)
 );
 
 CREATE TABLE voters (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    ticket VARCHAR(255) NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT,
     election_id INT NOT NULL,
-    FOREIGN KEY (election_id) REFERENCES elections(id)
+    ticket VARCHAR(255) NOT NULL UNIQUE,
+    CONSTRAINT voters_pk PRIMARY KEY (id),
+    CONSTRAINT voter_election FOREIGN KEY (election_id) REFERENCES elections(id)
+);
+
+CREATE TABLE votes (
+    id INT AUTO_INCREMENT,
+    candidate_id INT NOT NULL,
+    voter_id INT NOT NULL,
+    CONSTRAINT votes_pk PRIMARY KEY (id),
+    CONSTRAINT vote_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id),
+    CONSTRAINT vote_voter FOREIGN KEY (voter_id) REFERENCES voters(id)
 );
